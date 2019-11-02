45c0bdb840552dd7655ba0b1f768c9edb885f3b1
Christian Jensen <>
2019-11-02 15:32:32 -0500

    added nav-menu code

--- algorithim-bias/src/app/app-routing.module.ts
+++ algorithim-bias/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProblemComponent } from './problem/problem.component';
+import { CurrentlyAddressComponent } from './currently-address/currently-address.component';
+import { SolutionComponent } from './solution/solution.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'problem',
+    component: ProblemComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'currently-address',
+    component: CurrentlyAddressComponent
+  },
+  {
+    path: 'solution',
+    component: SolutionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- algorithim-bias/src/app/nav-menu/nav-menu.component.ts
+++ algorithim-bias/src/app/nav-menu/nav-menu.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
+  isExpanded = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+  collapse() {
+    this.isExpanded = false;
+  }
 
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
